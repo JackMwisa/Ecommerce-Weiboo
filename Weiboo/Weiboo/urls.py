74bf2b4c4502d8db8b93b36ca6eb6c62760522e6
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2025-07-28 13:19:29 +0300

    update

--- Weiboo/Weiboo/urls.py
+++ Weiboo/Weiboo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from Weiboo import homeViews
 from Weiboo import blogViews
 from Weiboo import pagesViews
@@ -71,3 +73,5 @@ urlpatterns = [
 
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
